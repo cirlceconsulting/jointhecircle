8385096b63d15307782052c9ec4fc72454fc22e8
Francis Guinto <francis.guinto@gmail.com>
2023-08-15 12:47:18 -0400

    white theme

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,26 @@ document.getElementById("email-form").onsubmit = function (e) {
     document.getElementById("lead-form").style.display = 'none';
     document.getElementById("thanks").style.display = 'flex';
 }
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
